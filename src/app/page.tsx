e5af3b381accc91c398036b2772c4ea755c2541f
nivr <4402120+nivr@users.noreply.github.com>
2024-03-13 00:22:48 +0100

    testing events

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,24 @@
 import Image from "next/image";
+//import Amplify from 'aws-amplify';
+import { Amplify } from "aws-amplify";
+import awsmobile from '../aws-exports';
+//import { record } from 'aws-amplify/analytics';
+import { configureAutoTrack } from 'aws-amplify/analytics';
+Amplify.configure(awsmobile);
+
+configureAutoTrack({
+  // REQUIRED, turn on/off the auto tracking
+  enable: true,
+  // REQUIRED, the event type, it's one of 'event', 'pageView' or 'session'
+  type: 'pageView',
+  // OPTIONAL, additional options for the tracked event.
+  options: {
+    // OPTIONAL, the attributes of the event
+    attributes: {
+      customizableField: 'attr'
+    }
+  }
+});
 
 export default function Home() {
   return (
