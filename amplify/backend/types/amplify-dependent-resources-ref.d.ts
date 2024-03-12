a7240f5fb25910f8ddf221bc82f803bbb128b1ef
nivr <4402120+nivr@users.noreply.github.com>
2024-03-12 22:40:32 +0100

    Initial commit

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,15 @@
+export type AmplifyDependentResourcesAttributes = {
+  "analytics": {
+    "amplifyanalytics": {
+      "Id": "string",
+      "Region": "string",
+      "appName": "string"
+    }
+  },
+  "auth": {
+    "amplifyanalytics": {
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string"
+    }
+  }
+}
