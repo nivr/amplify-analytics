d99aa747e471fef70c0ca892f87cce5e7f5fd9ac
nivr <4402120+nivr@users.noreply.github.com>
2024-03-13 08:47:58 +0100

    adjustevent

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ import { Amplify } from "aws-amplify";
 import awsmobile from '../aws-exports';
 //import { record } from 'aws-amplify/analytics';
 import { configureAutoTrack } from 'aws-amplify/analytics';
+import { record } from 'aws-amplify/analytics';
 Amplify.configure(awsmobile);
 
 configureAutoTrack({
@@ -16,22 +17,20 @@ configureAutoTrack({
     // OPTIONAL, the attributes of the event
     attributes: {
       customizableField: 'attr'
+    },
+    appType: 'singlePage',
+
+    // OPTIONAL, provide the URL for the event.
+    urlProvider:  () => {
+      // the default function
+      return window.location.origin + window.location.pathname;
     }
   }
 });
 
-configureAutoTrack({
-  // REQUIRED, turn on/off the auto tracking
-  enable: true,
-  // REQUIRED, the event type, it's one of 'event', 'pageView' or 'session'
-  type: 'session',
-  // OPTIONAL, additional options for the tracked event.
-  options: {
-    // OPTIONAL, the attributes of the event
-    attributes: {
-      customizableField: 'attr'
-    }
-  }
+record({
+  name: 'albumVisit',
+  attributes: { genre: '', artist: '' },
 });
 
 export default function Home() {
